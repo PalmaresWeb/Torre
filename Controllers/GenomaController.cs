f7c3faf8e2d5e075ae06d9196367ca30829ef127
PalmaresWeb <fernandopalazzi@hotmail.com>
2021-12-23 09:48:42 -0300

    Add files via upload

---
+++ Controllers/GenomaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Torre.Controllers
+{
+}
+    //[ApiController]
+    //[Route("[controller]")]
+    //public class GenomaController : ControllerBase
+    //{
+    //    [HttpGet]
+    //}
+    //    //public IActionResult Get()
+        //{
+        //    GenomaController rpCli = new GenomaController);
+        //    return Ok(rpCli.ObtenerCliente());
+        //}
+
+        //public GenomaController ObtenerCliente()
+        //{
+            
+             
+        //}
+
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<GenomaController> _logger;
+
+        //public GenomaController(ILogger<GenomaController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet]
+        //public IEnumerable<Genoma> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new GenomaController
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    });
+        //    .ToArray();
+    
+
+
